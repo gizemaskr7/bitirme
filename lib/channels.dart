df4ed57733588ad6cfdd041ee5ccaa6d9c5fb2c3
gizemaskr7 <gizemaskr7@gmail.com>
2024-04-29 10:50:47 +0300

    Yeni Kanallar Yüklendi

--- lib/channels.dart
+++ lib/channels.dart
@@ -9,25 +9,43 @@ class Channels {
     {'channel_name':'TRT 1','channel_url':'https://tr.canlitv.cam/yayin/trt-1-geo','channel_logo':'https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/TRT_1_logo_%282021-%29.svg/1200px-TRT_1_logo_%282021-%29.svg.png','channel_category':'Genel'},
     {'channel_name':'TV8','channel_url':'https://tr.canlitv.cam/yayin/tv8-geo','channel_logo':'https://upload.wikimedia.org/wikipedia/tr/6/68/Tv8_Yeni_Logo.png','channel_category':'Genel'},
     {'channel_name':'TLC','channel_url':'https://tr.canlitv.cam/yayin/tlc-tv-izle','channel_logo':'https://upload.wikimedia.org/wikipedia/commons/a/af/TLC-Logo_2016.png','channel_category':'Genel'},
+    {'channel_name':'NTV','channel_url':'https://tr.canlitv.cam/yayin/ntv-izle-3','channel_logo':'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/NTV_%28Turkey%29_logo.svg/768px-NTV_%28Turkey%29_logo.svg.png','channel_category':'Genel'},
+    {'channel_name':'TV 8,5','channel_url':'https://tr.canlitv.cam/yayin/tv8-5-geo','channel_logo':'https://upload.wikimedia.org/wikipedia/commons/2/2a/TV_8.5logo.png','channel_category':'Genel'},
+    {'channel_name':'a2 TV','channel_url':'https://tr.canlitv.cam/yayin/a2tvgeo','channel_logo':'https://upload.wikimedia.org/wikipedia/tr/e/e8/A2_logosu.jpg','channel_category':'Genel'},
+    {'channel_name':'CNN Turk','channel_url':'https://tr.canlitv.cam/yayin/cnn-geo-4','channel_logo':'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/CNN_T%C3%BCrk_logo.svg/2560px-CNN_T%C3%BCrk_logo.svg.png','channel_category':'Genel'},
 
     {'channel_name':'TRT Haber','channel_url':'https://tr.canlitv.cam/yayin/trt-haber-izle','channel_logo':'https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/TRT_Haber_logo_%282013-2020%29.svg/2560px-TRT_Haber_logo_%282013-2020%29.svg.png','channel_category':'Haber'},
     {'channel_name':'A Haber','channel_url':'https://tr.canlitv.cam/yayin/a-haber-izle','channel_logo':'https://upload.wikimedia.org/wikipedia/commons/7/7c/Ahaber_Logo.png','channel_category':'Haber'},
     {'channel_name':'HaberTurk','channel_url':' https://tr.canlitv.cam/yayin/haberturk-izle','channel_logo':'https://upload.wikimedia.org/wikipedia/commons/7/78/Haberturk_logo.png','channel_category':'Haber'},
     {'channel_name':'Haber Global','channel_url':'https://tr.canlitv.cam/yayin/habergloooooo','channel_logo':'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Haber_Global_logo.svg/2560px-Haber_Global_logo.svg.png','channel_category':'Haber'},
     {'channel_name':'ABC News Australia','channel_url':'https://tr.canlitv.cam/yayin/abc-news-australia','channel_logo':'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/ABC_News_%282017%29.svg/2560px-ABC_News_%282017%29.svg.png','channel_category':'Haber'},
+    {'channel_name':'A News','channel_url':'https://tr.canlitv.cam/yayin/a-news-izle','channel_logo':'https://upload.wikimedia.org/wikipedia/commons/3/37/Anews.png','channel_category':'Haber'},
+    {'channel_name':'Halk TV','channel_url':'https://tr.canlitv.cam/yayin/halk-tv-izle-2','channel_logo':'https://upload.wikimedia.org/wikipedia/commons/4/42/Halk_TV_logo.png','channel_category':'Haber'},
+    {'channel_name':'TV 100','channel_url':'https://tr.canlitv.cam/yayin/tv100000','channel_logo':'https://upload.wikimedia.org/wikipedia/tr/0/0f/TV100_logo.png','channel_category':'Haber'},
+    {'channel_name':'TGRT Haber','channel_url':'https://tr.canlitv.cam/yayin/tgrt-haber-geo','channel_logo':'https://upload.wikimedia.org/wikipedia/commons/e/e1/TGRT_Haber_logosu.png','channel_category':'Haber'},
 
     {'channel_name':'Minika Go','channel_url':'https://tr.canlitv.cam/yayin/minika-go-izle-2','channel_logo':'https://upload.wikimedia.org/wikipedia/commons/c/c9/MinikaGO_Logo.png','channel_category':'Çizgi Film'},
     {'channel_name':'TRT Çocuk','channel_url':'https://tr.canlitv.cam/yayin/trt-cocuk-izle','channel_logo':'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/TRT_%C3%87ocuk_logo_%282021%29.svg/1280px-TRT_%C3%87ocuk_logo_%282021%29.svg.png','channel_category':'Çizgi Film'},
+    {'channel_name':'Cartoon Network','channel_url':'https://tr.canlitv.cam/yayin/cartoongeo','channel_logo':'https://1000logos.net/wp-content/uploads/2021/05/Cartoon-Network-logo.png','channel_category':'Çizgi Film'},
 
     {'channel_name':'TRT Belgesel','channel_url':'https://tr.canlitv.cam/yayin/trt-belgesel-izle-2','channel_logo':'https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/TRT_Belgesel_logo_%282019-%29.svg/2560px-TRT_Belgesel_logo_%282019-%29.svg.png','channel_category':'Belgesel'},
+    {'channel_name':'Yaban TV','channel_url':'https://tr.canlitv.cam/yayin/yaban-tv-izle-2','channel_logo':'https://upload.wikimedia.org/wikipedia/commons/f/f6/Yaban_tv_logo_new.png','channel_category':'Belgesel'},
+    {'channel_name':'DMAX','channel_url':'https://tr.canlitv.cam/yayin/d-max-izle','channel_logo':'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/DMAX_-_Logo_2016.svg/1200px-DMAX_-_Logo_2016.svg.png','channel_category':'Belgesel'},
 
     {'channel_name':'CBC Sport','channel_url':'https://tr.canlitv.cam/yayin/cbc-geos','channel_logo':'https://upload.wikimedia.org/wikipedia/az/archive/0/04/20200227202418%21CBC_Sport_TV_loqo.png','channel_category':'Spor'},
     {'channel_name':'TRT Spor 2','channel_url':'https://tr.canlitv.cam/yayin/trtsporyilgeo2','channel_logo':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdKNBFrs1aRje1DJgBBLg-CCYLeGVvSf2AUHuLk7zM&s','channel_category':'Spor'},
     {'channel_name':'A Spor','channel_url':'https://tr.canlitv.cam/yayin/a-spor-izle-4','channel_logo':'https://upload.wikimedia.org/wikipedia/tr/archive/e/e9/20140920094552%21A_Spor_logosu.png','channel_category':'Spor'},
     {'channel_name':'beIN Sports Haber','channel_url':'https://tr.canlitv.cam/yayin/beins-geo','channel_logo':'https://media04.ligtv.com.tr/img/news/2020/6/12/super-lig-heyecani-bein-sports-haberde/748_416/beIN.jpg','channel_category':'Spor'},
+    {'channel_name':'GS TV','channel_url':'https://tr.canlitv.cam/yayin/gs-tv-izle-2','channel_logo':'https://upload.wikimedia.org/wikipedia/tr/a/a1/Gstv-yeni.PNG','channel_category':'Spor'},
+    {'channel_name':'FB TV','channel_url':'https://tr.canlitv.cam/yayin/fbtvvvvvvv','channel_logo':'https://upload.wikimedia.org/wikipedia/tr/thumb/0/01/Fenerbah%C3%A7e_TV_Logo.jpg/600px-Fenerbah%C3%A7e_TV_Logo.jpg','channel_category':'Spor'},
+    {'channel_name':'TRT Spor Yıldız','channel_url':'https://tr.canlitv.cam/yayin/trtsporyilgeo','channel_logo':'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/TRT_Spor_Y%C4%B1ld%C4%B1z_Logo.svg/2560px-TRT_Spor_Y%C4%B1ld%C4%B1z_Logo.svg.png','channel_category':'Spor'},
+    {'channel_name':'Tivibu Spor','channel_url':'https://tr.canlitv.cam/yayin/tivibu-spor','channel_logo':'https://upload.wikimedia.org/wikipedia/tr/3/36/Tivibu_spor_logosu.jpg','channel_category':'Spor'},
 
     {'channel_name':'Power TV','channel_url':'https://www.artidijitalmedya.com/embed/tv/powertv/','channel_logo':'https://upload.wikimedia.org/wikipedia/tr/b/b5/Power_TV_logosu.png','channel_category':'Müzik'},
     {'channel_name':'Power Türk TV','channel_url':'https://www.artidijitalmedya.com/embed/tv/powerturktv/','channel_logo':'https://upload.wikimedia.org/wikipedia/tr/a/a8/Powert%C3%BCrk_TV_logosu.png','channel_category':'Müzik'},
+    {'channel_name':'KRAL POP TV','channel_url':'https://tr.canlitv.cam/yayin/kral-pop-tv-izle','channel_logo':'https://upload.wikimedia.org/wikipedia/tr/9/9f/Kral_Pop_logosu.png','channel_category':'Müzik'},
+    {'channel_name':'Number1 TV','channel_url':'https://tr.canlitv.cam/yayin/number1-izle','channel_logo':'https://upload.wikimedia.org/wikipedia/commons/4/40/%22Number_one_tv_logo%22.png','channel_category':'Müzik'},
+    {'channel_name':'Number1 TURK TV','channel_url':'https://tr.canlitv.cam/yayin/number1-turk-izle','channel_logo':'https://www.numberone.com.tr/wp-content/uploads/2015/12/turk-tv-logo.jpg','channel_category':'Müzik'},
 
 
   ];
